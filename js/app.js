a09c2e49b324f2916b045eb32f268c146286d69e
MichelleSV <seguilvilchezmichelle@gmail.com>
2016-08-25 16:49:34 -0500

    numero positivo o negativo

---
+++ js/app.js
@@ -0,0 +1,7 @@
+var numero = parseInt(prompt("Escribe un número"));
+if( numero < 0 ){
+		document.write("Tu número es negativo " + numero);
+	}
+else{
+    document.write("Tu número es positivo " + numero);
+};
